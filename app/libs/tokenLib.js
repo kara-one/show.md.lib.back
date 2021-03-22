fc637ce0d8e300583ed5927e7f3e68e9b9bcef2d
Serhii Karavashkin <ksawl@narod.ru>
2021-03-22 11:43:50 +0200

    ref token middleware

--- app/libs/tokenLib.js
+++ app/libs/tokenLib.js
@@ -1,11 +1,18 @@
 const jwt = require('jsonwebtoken');
 const secret = process.env.TOKEN_SECRET || 'TOKEN_SECRET';
+const { errorWrap } = require(global.pathLib.fromRoot('/app/libs/errorLib'));
 
 module.exports = {
     tokenGenerate(payload) {
         return jwt.sign(payload, secret, { expiresIn: '24h' });
     },
     tokenVerify(token) {
-        return jwt.verify(token, secret);
+        return jwt.verify(token, secret, (err, decoded) => {
+            if (err) {
+                return errorWrap('token', token, 'Token error');
+            }
+
+            return decoded;
+        });
     },
 };
