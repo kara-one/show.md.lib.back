2846f68e8de4011d78a8b97f4ac9f66c26b28ea0
Serhii Karavashkin <ksawl@narod.ru>
2021-03-22 11:44:32 +0200

    ref auth middleware

--- system/server/express/middlewares/authMiddleware.js
+++ system/server/express/middlewares/authMiddleware.js
@@ -1,6 +1,4 @@
-const jwt = require('jsonwebtoken');
-
-const { secret } = require(global.pathLib.fromRoot('/app/libs/jwt/config'));
+const { tokenVerify } = require(global.pathLib.fromRoot('/app/libs/tokenLib'));
 
 module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS') {
@@ -8,12 +6,22 @@ module.exports = (req, res, next) => {
     }
 
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        if (!token) {
-            return res.status(400).json({ message: 'User is not auth2' });
+        if (
+            !req.headers.authorization ||
+            !req.headers.authorization.split(' ')[1]
+        ) {
+            return res.status(400).json({ message: 'User is not auth' });
         }
 
-        req.user = jwt.verify(token, secret);
+        const token = req.headers.authorization.split(' ')[1];
+        const checkToken = tokenVerify(token);
+        if (checkToken.errors) {
+            return res
+                .status(400)
+                .json({ message: 'User is not auth', errors: checkToken.errors });
+        }
+        
+        req.user = checkToken;
 
         next();
     } catch (e) {
